d978f162b13cd300c7626747fd14995a581d7ff9
graikos <raikosgiorgos7@gmail.com>
2021-12-30 16:53:41 +0200

    Fixed README; added script

--- scripts/mass_data.py
+++ scripts/mass_data.py
@@ -1,62 +1,40 @@
 import json
 import socket
 from sys import argv
+import os
+import sys
+sys.path.append(".")
+
 
 with open(argv[1]) as f:
     data = json.load(f)
 
+os.chdir("..")
+from src import utils
+
 if argv[2] in ("insert", "i"):
     req = "find_and_store_key"
     req_body = lambda e: {"key": e, "value": data[e]}
+    print("Inserting keys...")
 elif argv[2] in ("lookup", "l"):
     req = "find_key"
     req_body = lambda e: {"key": e}
+    print("Looking up keys...")
 else:
     print("Expected request type (i, l) as second argument.")
     exit(1)
 
-def create_request(header_dict, body_dict):
-    """
-    Creates request from passed header and body
-    :param header_dict: dictionary of header
-    :param body_dict: dictionary of body
-    :return:
-    """
-    request_dict = {"header": header_dict, "body": body_dict}
-    request_msg = json.dumps(request_dict, indent=2)
 
-    return request_msg
-
-def ask_peer(peer_addr, req_type, body_dict, return_json=True):
-    """
-    Edited version of ask_peer for general use outside Node
-    Sends a request and returns the response
-    :param peer_addr: (IP, port) of peer
-    :param req_type: type of request for request header
-    :param body_dict: dictionary of body
-    :param return_json: determines if json or string response should be returned
-    :return: string response of peer
-    """
-
-    request_msg = create_request({"type": req_type}, body_dict)
-
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
-        client.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        client.settimeout(5)
-        try:
-            client.connect(peer_addr)
-            client.sendall(request_msg.encode())
-            data = client.recv(1024).decode()
-        except (socket.error, socket.timeout):
-            return None
-
-    if not data:
-        return None
-
-    return data if not return_json else json.loads(data)
-
+failed_req = 0
+total_req = 0
 for event in data:
-    ask_peer(("", 9150), req, req_body(event))
+    response = utils.ask_peer(("", 9150), req, req_body(event))
+
+    if not response or response["header"]["status"] not in (200, 300):
+        failed_req += 1
+    total_req += 1
+
+    print(f"\rFail percentage: {failed_req*100/total_req}%" + 20*" ", end="")
 
 
 
